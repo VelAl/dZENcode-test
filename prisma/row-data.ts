dbb6fe89f24500c353a7c8e4b5a1cce7afa95a83
Alex <welmozhko88@gmail.com>
2025-05-21 13:23:42 +0300

    Completed Prisma migrations. Added prisma seed

--- prisma/row-data.ts
+++ prisma/row-data.ts
@@ -1,35 +1,11 @@
 import { T_Order, T_Product } from "@/app-types";
 
-export const orders: T_Order[] = [
-  {
-    id: 1,
-    title: "Order 1",
-    date: "2017-06-29T12:09:33",
-    description: "desc",
-    products: [],
-  },
-  {
-    id: 2,
-    title: "Order 2",
-    date: "2017-06-29 12:09:33",
-    description: "desc",
-    products: [],
-  },
-  {
-    id: 3,
-    title: "Order 3",
-    date: "2017-06-29 12:09:33",
-    description: "desc",
-    products: [],
-  },
-];
-
 export const products: T_Product[] = [
   {
     id: 1,
     serialNumber: 1234,
     isNew: true,
-    photo: "pathToFile.jpg",
+    photo: "../public/product-images/monitor_1.avif",
     title: "Product 1",
     type: "Monitors",
     specification: "Specification 1",
@@ -41,14 +17,14 @@ export const products: T_Product[] = [
       { value: 600, symbol: "USD", isDefault: false },
       { value: 2600, symbol: "UAH", isDefault: true },
     ],
-    order: 1,
+    order: 0,
     date: "2017-06-29T12:09:33",
   },
   {
     id: 2,
-    serialNumber: 1234,
+    serialNumber: 1235,
     isNew: false,
-    photo: "pathToFile.jpg",
+    photo: "../public/product-images/monitor_2.avif",
     title: "Product 2",
     type: "Monitors",
     specification: "Specification 2",
@@ -60,7 +36,31 @@ export const products: T_Product[] = [
       { value: 600, symbol: "USD", isDefault: false },
       { value: 2600, symbol: "UAH", isDefault: true },
     ],
-    order: 2,
+    order: 0,
     date: "2017-06-29T12:09:33",
+  },
+];
+
+export const orders: T_Order[] = [
+  {
+    id: 1,
+    title: "Order 1",
+    date: "2017-06-29T12:09:33",
+    description: "desc",
+    products,
+  },
+  {
+    id: 2,
+    title: "Order 2",
+    date: "2017-06-29 12:09:33",
+    description: "desc",
+    products,
+  },
+  {
+    id: 3,
+    title: "Order 3",
+    date: "2017-06-29 12:09:33",
+    description: "desc",
+    products,
   },
 ];
